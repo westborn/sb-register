07a2eec0d4a883db9079ea385d4d2f9d33cb2918
Westborn <george@westborn.com.au>
2021-03-10 13:30:43 +1100

    dependency: resolve security issues

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -67,11 +67,9 @@
 
       <v-layout row>
         <v-flex xs6 offset-xs1>
-          <v-btn
-            @click="submit"
-            class="primary"
-            :disabled="!isValid || sendingToServer"
-          >Submit this Registration?</v-btn>
+          <v-btn @click="submit" class="primary" :disabled="!isValid || sendingToServer"
+            >Submit this Registration?</v-btn
+          >
           <div v-if="sendingToServer">
             <v-progress-linear :indeterminate="true"></v-progress-linear>
           </div>
@@ -82,8 +80,8 @@
     <v-layout justify-center>
       <v-dialog v-model="dialog" persistent max-width="390">
         <v-card>
-          <v-card-title class="headline">{{dialogHeader}}</v-card-title>
-          <v-card-text>{{dialogMessages}}</v-card-text>
+          <v-card-title class="headline">{{ dialogHeader }}</v-card-title>
+          <v-card-text>{{ dialogMessages }}</v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="red darken-1" text @click="dialog = false">OK</v-btn>
@@ -95,76 +93,72 @@
 </template>
 
 <script>
-import { postData } from "../api/services";
+import { postData } from '../api/services'
 
 export default {
   data: () => ({
-    registerURL:
-      "https://script.google.com/macros/s/AKfycbxo2AF_dFaA7mALFpgiX_h6INNrcToPCvbDJX0ZB_ClIokJdKMS/exec",
+    registerURL: 'https://script.google.com/macros/s/AKfycbxo2AF_dFaA7mALFpgiX_h6INNrcToPCvbDJX0ZB_ClIokJdKMS/exec',
     sendingToServer: false,
     isValid: false,
     isError: false,
     dialog: false,
-    dialogHeader: "",
-    dialogMessages: "",
+    dialogHeader: '',
+    dialogMessages: '',
     form: {
       firstName: null,
       lastName: null,
       phone: null,
       email: null,
-      postcode: null
+      postcode: null,
     },
     nameRules: [
-      v => !!v || "Name is required",
-      v => (v && v.length >= 2) || "You need to enter at least 2 characters"
+      (v) => !!v || 'Name is required',
+      (v) => (v && v.length >= 2) || 'You need to enter at least 2 characters',
     ],
-    emailRules: [
-      v => !!v || "E-mail is required",
-      v => /.+@.+/.test(v) || "E-mail must be valid"
-    ]
+    emailRules: [(v) => !!v || 'E-mail is required', (v) => /.+@.+/.test(v) || 'E-mail must be valid'],
   }),
   methods: {
     submit() {
       if (this.$refs.registerForm.validate()) {
-        this.sendFormData();
+        this.sendFormData()
       } else {
-        console.log("where are we?");
-        this.submitError("Please contact us via email regarding this entry");
+        console.log('where are we?')
+        this.submitError('Please contact us via email regarding this entry')
       }
     },
     async sendFormData() {
-      this.sendingToServer = true;
-      const { ok, error, data } = await postData(this.registerURL, this.form);
-      this.sendingToServer = false;
+      this.sendingToServer = true
+      const { ok, error, data } = await postData(this.registerURL, this.form)
+      this.sendingToServer = false
       if (ok) {
-        if (data.result === "ok") {
-          this.submitSuccess(data.emailKey);
-          localStorage.setItem("currentEmailKey", data.emailKey);
+        if (data.result === 'ok') {
+          this.submitSuccess(data.emailKey)
+          localStorage.setItem('currentEmailKey', data.emailKey)
         } else {
-          this.submitError(data.error);
+          this.submitError(data.error)
         }
       } else {
-        this.submitError(error);
+        this.submitError(error)
       }
     },
     submitSuccess(result) {
-      this.isError = false;
-      this.showDialog("Registration Added", result);
-      this.isError = false;
+      this.isError = false
+      this.showDialog('Registration Added', result)
+      this.isError = false
 
       // if (this.dialog) this.$router.push({ path: "/" });
     },
     submitError(error) {
-      console.log("submitError");
-      console.log(error);
-      this.showDialog("Ooops - something went wrong", error);
-      this.isError = false;
+      console.log('submitError')
+      console.log(error)
+      this.showDialog('Ooops - something went wrong', error)
+      this.isError = false
     },
     showDialog(header, message) {
-      this.dialogHeader = header;
-      this.dialogMessages = message;
-      this.dialog = true;
-    }
-  }
-};
+      this.dialogHeader = header
+      this.dialogMessages = message
+      this.dialog = true
+    },
+  },
+}
 </script>
